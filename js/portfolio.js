9beb6ec9075c4058a52cea8a281c872aa38d908f
Dorian Kucharski <dorian.kucharski@gmail.com>
2017-11-12 21:06:37 +0100

    Add the rest of modals

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,4 +1,4 @@
-$('a[href*="#"]:not([href="#"],[href="#carousel-tictactoe"],[href="#carousel-profgoethe"],[href="#carousel-gotapp"],[href="#carousel-weatherapp"],[href="#carousel-blackjack"],[href="#carousel-pixelartmaker"])').click(function() {
+$('a[href*="#"]:not([href="#"],[href="#carousel-tictactoe"],[href="#carousel-profgoethe"],[href="#carousel-gotapp"],[href="#carousel-weatherapp"],[href="#carousel-blackjack"],[href="#carousel-pixelartmaker"],[href="#carousel-myresume"],[href="#carousel-todolist"],[href="#carousel-drukomatic"],[href="#carousel-robomaticon"])').click(function() {
   if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
     var target = $(this.hash);
     target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
